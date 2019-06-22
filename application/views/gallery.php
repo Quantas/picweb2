acf71be19a9596c77d2aee6b908e2128cdcbf93f
Andrew Landsverk <dewdew@gmail.com>
2019-06-22 17:23:00 -0500

    fix one last gallery bug

--- application/views/gallery.php
+++ application/views/gallery.php
@@ -7,7 +7,7 @@
 			foreach($pictures as $picture): ?>
         	<?php if ($count > 3) {?></tr><tr><?php $count=0; } ?>
             <td>
-        		<?php if(($picture->nsfw == 0 || ($picture->nsfw == 1 && $nsfw_pref == 1))){ ?><a href="<?php echo site_url()."displaypic.php?size=image&pic=".$picture->id; ?>" rel="lytebox[picwebGallery]" title="<?php echo $picture->name; ?>" ><img src="<?php if(!$using_ie){ echo "data:".$picture->type.";base64,".stripslashes($picture->thumb); }else{ echo site_url()."displaypic.php?size=thumb&pic=".$picture->id; } ?>" height="100px" alt="<?php echo $picture->name; ?>"  /></a><?php }else{ ?> <img src="<?php echo base_url();?>images/mature.png" /> <?php } ?>
+        		<?php if(($picture->nsfw == 0 || ($picture->nsfw == 1 && $nsfw_pref == 1))){ ?><a href="<?php echo "/displaypic.php?size=image&pic=".$picture->id; ?>" rel="lytebox[picwebGallery]" title="<?php echo $picture->name; ?>" ><img src="<?php if(!$using_ie){ echo "data:".$picture->type.";base64,".stripslashes($picture->thumb); }else{ echo site_url()."displaypic.php?size=thumb&pic=".$picture->id; } ?>" height="100px" alt="<?php echo $picture->name; ?>"  /></a><?php }else{ ?> <img src="<?php echo base_url();?>images/mature.png" /> <?php } ?>
        			<br/><a href="<?php echo site_url("gallery/pictureView/")."/".$picture->album_id."/".$picture->id; ?>"><?php if (strlen($picture->name) > 15 ) { echo substr($picture->name,0,15).'...'; } else { echo $picture->name; } ?></a>
         </td>
     <?php
